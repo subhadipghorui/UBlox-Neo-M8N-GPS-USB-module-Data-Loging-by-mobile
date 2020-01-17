1e05cde2fd3ae634e139c6c4aab57f7cd5cbcd14
Subhadip Ghorui <subhadipghorui105@gmail.com>
2020-01-17 17:23:41 +0530

    Corrected latlon formula

--- logUbloxm8n.py
+++ logUbloxm8n.py
@@ -6,13 +6,17 @@ path = input("Enter Log TXT File Name (log.txt): ")
 # Enter the file path
 myfile = open(path, encoding="cp437")
 
+
 # Creating a new file and closed it
-newFile = open("lat_lon_alt.csv", "w")
-newFile.writelines("lat, lon, alt" + "\n")
+newFileName = path[0:-4]+"_latlon.csv"
+newFile = open(newFileName, "w")
+newFile.writelines("slno, lat, lon, alt" + "\n")
 newFile.close()
 # Set the cursor to the 0 position
 myfile.seek(0)
 alt: float = 0
+slno: int = 0
+
 # Read line by line of the data
 for db_set in myfile:
     # print(db_set)
@@ -25,19 +29,25 @@ for db_set in myfile:
         # print(db_set)
         # print(val1)
         data = db_set.split(',')
-        lat = data[2]
-        lon = data[4]
+        latDeg = float(data[2][0:2])
+        latMin = float(data[2][2:9])/60
+        lat = latDeg + latMin
+
+        lonDeg = float(data[4][0:3])
+        lonMin = float(data[4][3:10])/60
+        lon = lonDeg + lonMin
         alt1 = float(data[9])
         alt2 = float(data[11])
-        alt = alt1 + alt2
-        # print(lat, lon, alt1, alt2)
+        alt = alt1 + alt2 
+        # print(lat, lon)
         # print(lat, lon, alt)
+        slno+=1
 
-        # # Write to a new file
-        newFile = open("lat_lon_alt.csv", "a")
-        newFile.writelines(str(lat) + "," + str(lon) + "," + str(alt) + "\n")
+        # Write to a new file
+        newFile = open(newFileName, "a")
+        newFile.writelines(str(slno)+","+str(lat) + "," + str(lon) + "," + str(alt) + "\n")
         newFile.close()
 
 
 print("Success")
-print("Open lat_lon_alt.csv file.")
+print("Open "+newFileName+" file.")
